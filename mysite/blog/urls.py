b57a7300fa464e722d05b38e6bbd58d16fa66cd5
maciekslawny <maciekslawny@gmail.com>
2021-05-27 21:12:29 +0200

    update

---
+++ mysite/blog/urls.py
@@ -0,0 +1,21 @@
+from django.urls import path
+from . import views
+
+app_name = 'blog'
+
+"""
+urlpatterns = [
+    #Widoki posta
+    path('', views.post_list, name='post_list'),
+    path('<int:year>/<int:month>/<int:day>/<slug:post>/',
+        views.post_detail,
+        name='post_detail'),
+]
+"""
+urlpatterns = [
+    #Widoki posta
+    path('', views.PostListView.as_view(), name='post_list'),
+    path('<int:year>/<int:month>/<int:day>/<slug:post>/',
+        views.post_detail,
+        name='post_detail'),
+]
--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,3 +1,43 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Post
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.views.generic import ListView
 
-# Create your views here.
+
+class PostListView(ListView):
+    queryset = Post.published.all()
+    context_object_name = 'posts'
+    paginate_by = 3
+    template_name = 'blog/post/list.html'
+
+def post_list(request):
+    posts = Post.published.all()
+    object_list = Post.published.all()
+    paginator = Paginator(object_list, 3) # 3 posty na każdej stronie
+    page = request.GET.get('page')  # wskazuje numer bieżącej strony
+
+    try:
+        posts = paginator.page(page)
+    except PageNotAnInteger:
+        #jeśli page nie jest liczbą całkowitą to pobierana jest pierwsza strona
+        posts = paginator.page(1) 
+    except EmptyPage:
+        #jeśli page jest większa niż numer ostatniej strony to pobierana jest ostatnia
+        posts = paginator.page(paginator.num_pages) 
+
+    return render(request,
+                    'blog/post/list.html',
+                    {'posts': posts,
+                     'page': page})
+
+
+
+def post_detail(request, year, month, day, post):
+    post = get_object_or_404(Post, slug=post,
+                                   status='published',
+                                   publish__year=year,
+                                   publish__month=month,
+                                   publish__day=day)
+    return render(request,
+                    'blog/post/detail.html',
+                    {'post': post})
