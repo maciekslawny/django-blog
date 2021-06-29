1ee4558edc712d88869a586e352969d49b55757c
maciekslawny <m.slawnicki.207@studms.ug.edu.pl>
2021-06-29 17:12:41 +0200

    update

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.utils import timezone
 from django.urls import reverse
 from django.contrib.auth.models import User
+from taggit.managers import TaggableManager
 
 
 class PublishedManager(models.Manager):
@@ -29,6 +30,7 @@ class Post(models.Model):
                                 default='draft')
     objects = models.Manager() # Menadżer domyślny
     published = PublishedManager()  # Menadżer niestandardowy 
+    tags = TaggableManager()
 
 
     class Meta:
